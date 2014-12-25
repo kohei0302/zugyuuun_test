26bf6ca628fa8255251c2f33672f5f6cd25fbdf5
kohei yamasaki <kohei0302@gmail.com>
2014-12-26 01:48:04 +0900

    [add]テスト

---
+++ app.js
@@ -0,0 +1,25 @@
+ZGN(function()
+{
+  // 17番ピンで動作させます
+  var ledPin = '17';
+
+  // TerminalのGPIOインスタンスを取得します
+  var gpio = ZGN.term('1').gpio;
+
+  // 指定ピンを出力に設定
+  gpio.pinMode(ledPin, ZGN.OUTPUT);
+
+  // ONボタンをクリック
+  $(document).on('click', '#on', function() {
+    gpio.digitalWrite(ledPin, ZGN.HIGH, function (){
+      console.log('HIGH');
+    }); // 点灯
+  });
+
+  // OFFボタンをクリック
+  $(document).on('click', '#off', function() {
+    gpio.digitalWrite(ledPin, ZGN.LOW, function (){
+      console.log('LOW');
+    }); // 消灯
+  });
+});
